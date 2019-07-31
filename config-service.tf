811b8ec066f679590b3c69aeaa3e769658136264
Jacek <jacek@ivolution.pl>
2019-07-31 17:05:40 +0200

    terraform 0.12 working

--- config-service.tf
+++ config-service.tf
@@ -5,27 +5,28 @@
 resource "aws_config_configuration_recorder_status" "main" {
   name       = "aws-config"
   is_enabled = true
-  depends_on = ["aws_config_delivery_channel.main"]
+  depends_on = [aws_config_delivery_channel.main]
 }
 
 resource "aws_config_delivery_channel" "main" {
   name           = "aws-config"
-  s3_bucket_name = "${var.config_logs_bucket}"
-  s3_key_prefix  = "${var.config_logs_prefix}"
+  s3_bucket_name = var.config_logs_bucket
+  s3_key_prefix  = var.config_logs_prefix
 
   snapshot_delivery_properties {
-    delivery_frequency = "${var.config_delivery_frequency}"
+    delivery_frequency = var.config_delivery_frequency
   }
 
-  depends_on = ["aws_config_configuration_recorder.main"]
+  depends_on = [aws_config_configuration_recorder.main]
 }
 
 resource "aws_config_configuration_recorder" "main" {
   name     = "aws-config"
-  role_arn = "${aws_iam_role.main.arn}"
+  role_arn = aws_iam_role.main.arn
 
   recording_group {
     all_supported                 = true
     include_global_resource_types = true
   }
 }
+
--- iam.tf
+++ iam.tf
@@ -1,15 +1,16 @@
 # Get the access to the effective Account ID in which Terraform is working.
-data "aws_caller_identity" "current" {}
+data "aws_caller_identity" "current" {
+}
 
 # Allows AWS Config IAM role to access the S3 bucket where AWS Config records
 # are stored.
 data "template_file" "aws_config_policy" {
-  template = "${file("${path.module}/iam-policies/aws-config-policy.tpl")}"
+  template = file("${path.module}/iam-policies/aws-config-policy.tpl")
 
   vars = {
-    config_logs_bucket = "${var.config_logs_bucket}"
-    config_logs_prefix = "${var.config_logs_prefix}"
-    account_id         = "${data.aws_caller_identity.current.account_id}"
+    config_logs_bucket = var.config_logs_bucket
+    config_logs_prefix = var.config_logs_prefix
+    account_id         = data.aws_caller_identity.current.account_id
   }
 }
 
@@ -34,22 +35,23 @@ data "aws_iam_policy_document" "aws-config-role-policy" {
 resource "aws_iam_role" "main" {
   name = "aws-config-role"
 
-  assume_role_policy = "${data.aws_iam_policy_document.aws-config-role-policy.json}"
+  assume_role_policy = data.aws_iam_policy_document.aws-config-role-policy.json
 }
 
 resource "aws_iam_policy_attachment" "managed-policy" {
   name       = "aws-config-managed-policy"
-  roles      = ["${aws_iam_role.main.name}"]
+  roles      = [aws_iam_role.main.name]
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSConfigRole"
 }
 
 resource "aws_iam_policy" "aws-config-policy" {
   name   = "aws-config-policy"
-  policy = "${data.template_file.aws_config_policy.rendered}"
+  policy = data.template_file.aws_config_policy.rendered
 }
 
 resource "aws_iam_policy_attachment" "aws-config-policy" {
   name       = "aws-config-policy"
-  roles      = ["${aws_iam_role.main.name}"]
-  policy_arn = "${aws_iam_policy.aws-config-policy.arn}"
+  roles      = [aws_iam_role.main.name]
+  policy_arn = aws_iam_policy.aws-config-policy.arn
 }
+
