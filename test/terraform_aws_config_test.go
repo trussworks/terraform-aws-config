27353c8f4b78250350c6dbbf2ba08b85a747df0a
Mojo Talantikite <mojo@redoxengine.com>
2019-11-14 13:05:41 -0500

    Add required_tags basic test

--- test/terraform_aws_config_test.go
+++ test/terraform_aws_config_test.go
@@ -37,3 +37,26 @@ func TestTerraformAwsConfig(t *testing.T) {
 	terraform.InitAndApply(t, terraformOptions)
 
 }
+
+func TestRequiredTags(t *testing.T) {
+	configName := fmt.Sprintf("aws-config-%s", strings.ToLower(random.UniqueId()))
+	expectedConfigLogsBucket := fmt.Sprintf("terratest-%s", configName)
+	awsRegion := "us-west-2"
+
+	terraformOptions := &terraform.Options{
+		TerraformDir: "../examples/required-tags/",
+		Vars: map[string]interface{}{
+			"region":             awsRegion,
+			"config_logs_bucket": expectedConfigLogsBucket,
+			"config_name":        configName,
+		},
+		EnvVars: map[string]string{
+			"AWS_DEFAULT_REGION": awsRegion,
+		},
+	}
+
+	defer terraform.Destroy(t, terraformOptions)
+	defer aws.EmptyS3Bucket(t, awsRegion, expectedConfigLogsBucket)
+
+	terraform.InitAndApply(t, terraformOptions)
+}
