c6adf4c6135f546cde325891b35955497c87eb1b
Michael Kania <dynamike@truss.works>
2020-03-02 11:15:48 -0800

    add types to variables

--- variables.tf
+++ variables.tf
@@ -41,86 +41,103 @@ variable "config_delivery_frequency" {
 
 variable "acm_days_to_expiration" {
   description = "Specify the number of days before the rule flags the ACM Certificate as noncompliant."
+  type        = number
   default     = 14
 }
 
 variable "password_require_uppercase" {
   description = "Require at least one uppercase character in password."
+  type        = bool
   default     = true
 }
 
 variable "password_require_lowercase" {
   description = "Require at least one lowercase character in password."
+  type        = bool
   default     = true
 }
 
 variable "password_require_symbols" {
   description = "Require at least one symbol in password."
+  type        = bool
   default     = true
 }
 
 variable "password_require_numbers" {
   description = "Require at least one number in password."
+  type        = bool
   default     = true
 }
 
 variable "password_min_length" {
   description = "Password minimum length."
+  type        = number
   default     = 14
 }
 
 variable "password_reuse_prevention" {
   description = "Number of passwords before allowing reuse."
+  type        = number
   default     = 24
 }
 
 variable "password_max_age" {
   description = "Number of days before password expiration."
+  type        = number
   default     = 90
 }
 
 variable "check_root_account_mfa_enabled" {
   description = "Enable root-account-mfa-enabled rule"
+  type        = bool
   default     = false
 }
 
 variable "check_guard_duty" {
   description = "Enable guardduty-enabled-centralized rule"
+  type        = bool
   default     = false
 }
 
 variable "check_rds_public_access" {
   description = "Enable rds-instance-public-access-check rule"
+  type        = bool
   default     = false
 }
 
 variable "check_multi_region_cloud_trail" {
   description = "Enable multi-region-cloud-trail-enabled rule"
+  type        = bool
   default     = false
 }
 
 variable "check_cloudtrail_enabled" {
   description = "Enable cloudtrail-enabled rule"
+  type        = bool
   default     = true
 }
 
 variable "check_cloud_trail_encryption" {
   description = "Enable cloud-trail-encryption-enabled rule"
+  type        = bool
   default     = false
 }
 
 variable "check_cloud_trail_log_file_validation" {
   description = "Enable cloud-trail-log-file-validation-enabled rule"
+  type        = bool
   default     = false
 }
 
 variable "check_eip_attached" {
   description = "Enable eip-attached rule"
+  type        = bool
   default     = false
 }
 
 variable "check_required_tags" {
   description = "Enable required-tags rule"
+  type        = bool
   default     = false
 }
 
@@ -138,36 +155,43 @@ variable "required_tags" {
 
 variable "check_instances_in_vpc" {
   description = "Enable instances-in-vpc rule"
+  type        = bool
   default     = true
 }
 
 variable "check_acm_certificate_expiration_check" {
   description = "Enable acm-certificate-expiration-check rule"
+  type        = bool
   default     = true
 }
 
 variable "check_iam_password_policy" {
   description = "Enable iam-password-policy rule"
+  type        = bool
   default     = true
 }
 
 variable "check_iam_group_has_users_check" {
   description = "Enable iam-group-has-users-check rule"
+  type        = bool
   default     = true
 }
 
 variable "check_iam_user_no_policies_check" {
   description = "Enable iam-user-no-policies-check rule"
+  type        = bool
   default     = true
 }
 
 variable "check_ec2_volume_inuse_check" {
   description = "Enable ec2-volume-inuse-check rule"
+  type        = bool
   default     = true
 }
 
 variable "check_approved_amis_by_tag" {
   description = "Enable approved-amis-by-tag rule"
+  type        = bool
   default     = false
 }
 
@@ -179,25 +203,30 @@ variable "ami_required_tag_key_value" {
 
 variable "check_ec2_encrypted_volumes" {
   description = "Enable ec2-encrypted-volumes rule"
+  type        = bool
   default     = true
 }
 
 variable "check_rds_storage_encrypted" {
   description = "Enable rds-storage-encrypted rule"
+  type        = bool
   default     = true
 }
 
 variable "check_rds_snapshots_public_prohibited" {
   description = "Enable rds-snapshots-public-prohibited rule"
+  type        = bool
   default     = true
 }
 
 variable "check_s3_bucket_public_write_prohibited" {
   description = "Enable s3-bucket-public-write-prohibited rule"
+  type        = bool
   default     = true
 }
 
 variable "check_cloudwatch_log_group_encrypted" {
   description = "Enable cloudwatch-log-group-encryption rule"
+  type        = bool
   default     = true
 }
